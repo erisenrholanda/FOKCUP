39ef37b7dfe7b2cd3681ba5086c7d71263dc9dea
unknown <erisen.rholanda@gmail.com>
2021-04-29 21:37:36 -0300

    upando arquivos que insistiam em não ir

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Site from './components/FOKCUP/index';
+import styles from './global.css';
+
+function App() {
+  return (
+    <div className="App" styles={styles}>
+      <Site />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/FOKCUP/index.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import style from './index.css';
+import Logo from './img/Logo.png'
+
+function Header() {
+  return(
+    <div style={style} className="body">
+      <header>
+        <li>
+            <a href="./">
+              <img src={Logo} alt="FOK" className="img" />
+            </a>
+          </li>
+      </header>
+      <main>
+        <span className='nomeTime'>FOK CUP</span>
+        <h1>Organização de Campeonatos de Free Fire</h1>
+        <nav>
+          <div className='box-contact'>
+            <a href="https://api.whatsapp.com/message/LKMSVGWEIOTEA1" className='link-contact'>
+              <img src="https://img.icons8.com/nolan/64/whatsapp.png" alt='WhatsApp'/>
+              <span>WhatsApp</span>
+            </a>
+          </div>
+          <div className='box-contact'>
+            <a href="https://www.instagram.com/fokgg/" className='link-contact'>
+              <img src="https://img.icons8.com/nolan/64/instagram-new.png" alt="Instagram" />
+              <span>Instagram</span>
+            </a>
+          </div>
+          {/* <div className='box-contact'>
+            <a href="http://www.youtube.com" className='link-contact'>
+              <img src="https://img.icons8.com/nolan/64/youtube-music.png" alt="Youtube" />
+              <span>Youtube</span>
+            </a>
+          </div>
+          <div className='box-contact'>
+              <a href="http://www.discord.com" className='link-contact'>
+                <img src="https://img.icons8.com/nolan/64/discord-logo.png" alt="Discord" />
+                <span>Discord</span>
+              </a>
+          </div> */}
+        </nav>
+      </main>
+      <footer>
+        <span></span>
+      </footer>
+    </div>
+  )
+}
+
+export default Header
